17af2e40850799be3aa9284ae231d7ef002b6fd7
decade <20120220@student.hcmus.edu.vn>
2025-02-08 01:19:56 +0700

    53

--- extended/src/gated.py
+++ extended/src/gated.py
@@ -36,7 +36,7 @@ class gMLPBlock(nn.Module):
 
 
 class gMLP(nn.Module):
-    def __init__(self, features, tokens, expand=4, num_layers=6):
+    def __init__(self, features, tokens, expand=1, num_layers=1):
         super().__init__()
         self.hidden = features * expand
         self.model = nn.Sequential(
