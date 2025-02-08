3d4abc29839cd0646814b3412362a39b2009508c
decade <20120220@student.hcmus.edu.vn>
2025-02-08 21:39:49 +0700

    76

--- extended/src/gated.py
+++ extended/src/gated.py
@@ -1,18 +1,33 @@
 from torch import nn
 from torch.nn import functional as F
+import torch
+
+
+class TriU(nn.Module):
+    def __init__(self, time_step):
+        super(TriU, self).__init__()
+        self.time_step = time_step
+        self.triU = nn.ParameterList([nn.Linear(i + 1, 1) for i in range(time_step)])
+
+    def forward(self, inputs):
+        x = self.triU[0](inputs[:, :, 0].unsqueeze(-1))
+        for i in range(1, self.time_step):
+            x = torch.cat([x, self.triU[i](inputs[:, :, 0 : i + 1])], dim=-1)
+        return x
 
 
 class SpatialGatingUnit(nn.Module):
     def __init__(self, hidden, tokens):
         super().__init__()
         self.norm = nn.LayerNorm(hidden)
-        self.spatial_proj = nn.Conv1d(tokens, tokens, kernel_size=1)
-        nn.init.constant_(self.spatial_proj.bias, 1.0)
+        self.spatial_proj = TriU(tokens)
 
     def forward(self, x):
         u, v = x.chunk(2, dim=-1)
         v = self.norm(v)
+        v = v.permute(0, 2, 1)
         v = self.spatial_proj(v)
+        v = v.permute(0, 2, 1)
         out = u * v
         return out
 
