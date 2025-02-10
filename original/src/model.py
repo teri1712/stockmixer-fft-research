02ada94a04e20493e0f68472e066c6f6ce909605
decade <20120220@student.hcmus.edu.vn>
2025-02-10 20:08:38 +0700

    106

--- original/src/model.py
+++ original/src/model.py
@@ -187,7 +187,7 @@ class NoGraphMixer(nn.Module):
         self.layer_norm_stock = nn.LayerNorm(stocks)
 
         self.dense3 = nn.Linear(stocks, hidden_dim)
-        self.gate = nn.Sigmoid()
+        self.gate = nn.Softmax(dim=-1)
 
     def forward(self, inputs):
         x = inputs
