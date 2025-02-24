663e896eb8418fbc151c1c894debdd8150c3a3d5
decade <20120220@student.hcmus.edu.vn>
2025-02-25 06:27:41 +0700

    183

--- original/src/preprocess.py
+++ original/src/preprocess.py
@@ -60,12 +60,12 @@ def calculate_macd(prices, fast=12, slow=26, signal=9):
 
 def append_technical_indicators(stock_prices):
     close_prices = stock_prices[:, :, 3]
-    #     rsi = np.apply_along_axis(calculate_rsi, axis=1, arr=close_prices)
-    #     rsi = np.expand_dims(rsi, axis=-1)
-    macd = np.apply_along_axis(
-        calculate_macd,
-        axis=1,
-        arr=close_prices,
-    )
+    rsi = np.apply_along_axis(calculate_rsi, axis=1, arr=close_prices)
+    rsi = np.expand_dims(rsi, axis=-1)
+    # macd = np.apply_along_axis(
+    #     calculate_macd,
+    #     axis=1,
+    #     arr=close_prices,
+    # )
 
-    return np.concatenate([stock_prices, macd], axis=2)
+    return np.concatenate([stock_prices, rsi], axis=2)
