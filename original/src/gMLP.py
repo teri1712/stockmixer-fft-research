25a4432692e6c6cb035eb19e5ebfe1f3e9c93d1a
decade <20120220@student.hcmus.edu.vn>
2025-03-10 18:27:29 +0700

    280

--- original/src/gMLP.py
+++ original/src/gMLP.py
@@ -60,7 +60,7 @@ class gMLP(nn.Module):
             seq_len,
             input_dim,
             hidden_dim=128,
-            depth=3,
+            depth=6,
             dropout_rate=0.1
     ):
         super().__init__()
