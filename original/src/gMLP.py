ac08e0ac24205f37c60ae38fae022ebd871471d6
decade <20120220@student.hcmus.edu.vn>
2025-03-12 01:20:28 +0700

    321

--- original/src/gMLP.py
+++ original/src/gMLP.py
@@ -34,7 +34,7 @@ class gMLPBlock(nn.Module):
         self.channel_proj1 = nn.Linear(input_dim, hidden_dim * 2)
         self.sgu = SigmoidGatingUnit(hidden_dim, seq_len)
         self.channel_proj2 = nn.Linear(hidden_dim, input_dim)
-        self.dropout = nn.Dropout(dropout_rate)
+        # self.dropout = nn.Dropout(dropout_rate)
         self.acv = nn.Hardswish()
 
     def forward(self, x):
@@ -50,7 +50,7 @@ class gMLPBlock(nn.Module):
 
         # Second projection and dropout
         x = self.channel_proj2(x)
-        x = self.dropout(x)
+        # x = self.dropout(x)
 
         # Add residual connection
         return x
