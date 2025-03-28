f55e79db2eab7b4a2144c6e1360dd5b748182d74
decade <20120220@student.hcmus.edu.vn>
2025-03-29 04:33:19 +0700

    400

--- original/src/param_search.sh
+++ original/src/param_search.sh
@@ -1,5 +1,5 @@
-market_values=("15" "20" "25" "32" "48" "64" "96" "128" "192" "256")
-depth_values=("5")
+market_values=("8" "16" "25" "32" "48" "64")
+depth_values=("2" "4" "6")
 
 for p1 in "${market_values[@]}"; do
   for p2 in "${depth_values[@]}"; do
